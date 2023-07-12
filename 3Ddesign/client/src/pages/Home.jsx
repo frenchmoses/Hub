bbfe1bfdc4bbd9fa0b8b9e295ccca867e81f6c3b
Austin <austinf.moses@outlook.com>
2023-07-12 13:31:52 -0700

    Working

--- 3Ddesign/client/src/pages/Home.jsx
+++ 3Ddesign/client/src/pages/Home.jsx
@@ -1,14 +1,8 @@
 import { motion,AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
-
 import state from '../store';
 import { CustomButton } from '../components';
-import {
-  headContainerAnimation,
-  headContentAnimation,
-  headTextAnimation,
-  slideAnimation
-} from '../config/motion';
+import { headContainerAnimation, headContentAnimation, headTextAnimation,slideAnimation } from '../config/motion';
 
 const Home = () => {
   const snap = useSnapshot(state);
